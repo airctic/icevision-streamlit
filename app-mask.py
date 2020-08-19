9a3e52c5a5993e5cace4cb16db86283bec18c4ad
ai-fast-track <farid@itecpedia.com>
2020-08-19 19:05:20 -0400

    with load_image

--- app-mask.py
+++ app-mask.py
@@ -1,9 +1,63 @@
 import streamlit as st
 import PIL, requests
 from mantisshrimp.all import *
+from PIL import Image
 
 WEIGHTS_URL = "https://mantisshrimp-models.s3.us-east-2.amazonaws.com/pennfundan_maskrcnn_resnet50fpn.zip"
 CLASS_MAP = datasets.pennfundan.class_map()
+
+pennfundan_images = [
+    "https://raw.githubusercontent.com/ai-fast-track/ice-streamlit/master/images/image1.png",
+    "https://raw.githubusercontent.com/ai-fast-track/ice-streamlit/master/images/image2.png",
+    "https://raw.githubusercontent.com/ai-fast-track/ice-streamlit/master/images/image3.png",
+    "https://raw.githubusercontent.com/ai-fast-track/ice-streamlit/master/images/image4.png",
+    "https://raw.githubusercontent.com/ai-fast-track/ice-streamlit/master/images/image5.png",
+    "https://raw.githubusercontent.com/ai-fast-track/ice-streamlit/master/images/image6.png",
+]
+
+
+@st.cache(show_spinner=False)
+def load_image_file(image_path):
+    """
+    Loads an Image file
+    """
+    image = Image.open(image_path)
+    image = image.convert("RGB")
+    image = np.array(image)
+    image = image / 255.0
+    image = image.astype(np.float32)
+    return image
+
+
+def random_image():
+    if st.button("Press Me!"):
+        # Just load an image from sample_images folder
+        random_image = random.choice(pennfundan_images)
+        image_url = st.text_input(label="Image url", value=random_image,)
+        # st.image(random_image)
+        # image = load_image_file(random_image)
+        flag = 1
+
+
+# This sidebar UI lets the user select model thresholds.
+def object_detector_ui():
+    st.sidebar.markdown("# Model Thresholds")
+    confidence_threshold = st.sidebar.slider(
+        "Confidence threshold", 0.0, 1.0, 0.5, 0.01
+    )
+    overlap_threshold = st.sidebar.slider("Overlap threshold", 0.0, 1.0, 0.3, 0.01)
+    return confidence_threshold, overlap_threshold
+
+
+def sidebar_ui():
+    st.sidebar.image("images/airctic-logo-medium.png")
+
+    page = st.sidebar.selectbox(
+        "Choose a dataset", ["PETS", "PennFundan", "Fridge Objects", "Raccoon"]
+    )  # pages
+
+    # Draw the UI element to select parameters for the YOLO object detector.
+    confidence_threshold, overlap_threshold = object_detector_ui()
 
 
 @st.cache(allow_output_mutation=True)
@@ -42,6 +96,7 @@ def show_prediction(img, pred):
         class_map=CLASS_MAP,
         denormalize_fn=denormalize_imagenet,
         show=True,
+        bbox=False,
     )
 
     # Grab image from the current matplotlib figure
@@ -53,12 +108,20 @@ def show_prediction(img, pred):
 
 
 def run_app():
-    st.title("MantisShrimp Demo App")
+    st.image("images/icevision.png")
+    # st.image("images/image3.png")
+    sidebar_ui()
 
     image_url = st.text_input(
         label="Image url",
         value="https://raw.githubusercontent.com/ai-fast-track/ice-streamlit/master/images/image1.png",
     )
+
+    if st.button("Press Me!"):
+        # Just load an image from sample_images folder
+        random_image = random.choice(pennfundan_images)
+        image_url = st.text_input(label="Image url", value=random_image,)
+    # random_image()
 
     model = load_model()
 
