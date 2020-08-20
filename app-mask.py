d39163eb849170e9ceedbb6d79b4337205f4c2f1
ai-fast-track <farid@itecpedia.com>
2020-08-20 10:21:38 -0400

    added bbox and airctic logo

--- app-mask.py
+++ app-mask.py
@@ -14,7 +14,17 @@ pennfundan_images = [
     "https://raw.githubusercontent.com/ai-fast-track/ice-streamlit/master/images/image4.png",
     "https://raw.githubusercontent.com/ai-fast-track/ice-streamlit/master/images/image5.png",
     "https://raw.githubusercontent.com/ai-fast-track/ice-streamlit/master/images/image6.png",
+    "https://i.cbc.ca/1.5510620.1585229177!/cumulusImage/httpImage/image.jpg_gen/derivatives/16x9_780/toronto-street-scene-covid-19.jpg",
 ]
+
+
+def sidebar_ui():
+    # st.sidebar.image("images/airctic-logo-medium.png")
+    st.sidebar.image("images/icevision-deploy-small.png")
+
+    # page = st.sidebar.selectbox(
+    #     "Choose a dataset", ["PETS", "PennFundan", "Fridge Objects", "Raccoon"]
+    # )  # pages
 
 
 # This sidebar UI lets the user select model thresholds.
@@ -23,15 +33,6 @@ def object_detector_ui():
     detection_threshold = st.sidebar.slider("Detection threshold", 0.0, 1.0, 0.5, 0.01)
     mask_threshold = st.sidebar.slider("Mask threshold", 0.0, 1.0, 0.5, 0.01)
     return detection_threshold, mask_threshold
-
-
-def sidebar_ui():
-    # st.sidebar.image("images/airctic-logo-medium.png")
-    st.sidebar.image("images/icevision-deploy-small.png")
-
-    page = st.sidebar.selectbox(
-        "Choose a dataset", ["PETS", "PennFundan", "Fridge Objects", "Raccoon"]
-    )  # pages
 
 
 @st.cache(allow_output_mutation=True)
@@ -70,14 +71,14 @@ def predict(
     return samples[0]["img"], preds[0]
 
 
-def show_prediction(img, pred):
+def show_prediction(img, pred, bbox=False, image_key="image_key"):
     show_pred(
         img=img,
         pred=pred,
         class_map=CLASS_MAP,
         denormalize_fn=denormalize_imagenet,
         show=True,
-        bbox=False,
+        bbox=bbox,
     )
 
     # Grab image from the current matplotlib figure
@@ -85,7 +86,7 @@ def show_prediction(img, pred):
     fig.canvas.draw()
     fig_arr = np.array(fig.canvas.renderer.buffer_rgba())
 
-    st.image(fig_arr, use_column_width=True)
+    st.image(fig_arr, use_column_width=True, image_key=image_key)
 
 
 def run_app():
@@ -95,26 +96,30 @@ def run_app():
     # Draw the threshold parameters for object detection model.
     detection_threshold, mask_threshold = object_detector_ui()
 
+    bbox = st.sidebar.checkbox(label="Bounding Box", value=False)
+    print("bbox", bbox)
+
+    st.sidebar.image("images/airctic-logo-medium.png")
+
     st.markdown("### ** Paste Your Image URL**")
     my_placeholder = st.empty()
 
-    image_url = my_placeholder.text_input(
-        label="",
-        value="https://raw.githubusercontent.com/ai-fast-track/ice-streamlit/master/images/image1.png",
-    )
+    index = 0
+    image_path = pennfundan_images[index]
+    image_url_key = f"image_url_key-{index}"
+    image_url = my_placeholder.text_input(label="", value=image_path, key=image_url_key)
 
     st.markdown("### **Or**")
     if st.button("Press Me!"):
         # Just load an image from sample_images folder
-        random_index = randrange(len(pennfundan_images))
-        random_image = pennfundan_images[random_index]
-        image_key = f"image{random_index}"
-        image_url = my_placeholder.text_input(
-            label="", value=random_image, key=image_key
-        )
+        index = randrange(len(pennfundan_images))
+        image_path = pennfundan_images[index]
+        image_url_key = f"image_url_key-{index}"
+        image_url = my_placeholder.text_input(label="", value=image_path, key=image_url_key)
 
     model = load_model()
 
+    image_key = f"image_key-{index}"
     if image_url:
         img, pred = predict(
             model=model,
@@ -122,7 +127,7 @@ def run_app():
             detection_threshold=detection_threshold,
             mask_threshold=mask_threshold,
         )
-        show_prediction(img=img, pred=pred)
+        show_prediction(img=img, pred=pred, bbox=bbox, image_key=image_key)
 
 
 if __name__ == "__main__":
